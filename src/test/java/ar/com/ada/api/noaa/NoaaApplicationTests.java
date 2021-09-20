87b653a4fe70124e2232f773419f230799e30551
MelinaMontes <you@example.com>
2021-09-20 20:43:44 -0300

    agrego html para que no devuelva un whiteLabel, sino un mensaje mas friendly

--- src/test/java/ar/com/ada/api/noaa/NoaaApplicationTests.java
+++ src/test/java/ar/com/ada/api/noaa/NoaaApplicationTests.java
@@ -2,7 +2,9 @@ package ar.com.ada.api.noaa;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.ComponentScan;
 
+//@ComponentScan(basePackages = {"src/main/java/ar/com/ada/api/noaa/controllers"})
 @SpringBootTest
 class NoaaApplicationTests {
 
